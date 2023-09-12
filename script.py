52889a85613df114095a388d7d35e9938c6504b3
jamescross813 <crossjames813@gmail.com>
2023-09-11 20:26:28 -0500

    write to new file

--- script.py
+++ script.py
@@ -1,2 +1,14 @@
 import csv
 import json
+
+compromised_users = []
+
+with open('passwords.csv') as password_file:
+  password_csv = csv.DictReader(password_file)
+  for info in password_csv:
+    password_row = info
+    compromised_users.append(password_row['Username'])
+
+with open('compromised_users.txt', 'w') as compromised_user_file:
+  for user in compromised_users:
+    compromised_user_file.write(user)
