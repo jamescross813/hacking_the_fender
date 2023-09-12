6d430b1b8566e83bc21912f59118135810a7d74c
jamescross813 <crossjames813@gmail.com>
2023-09-11 20:26:52 -0500

    create message for boss

--- script.py
+++ script.py
@@ -12,3 +12,10 @@ with open('passwords.csv') as password_file:
 with open('compromised_users.txt', 'w') as compromised_user_file:
   for user in compromised_users:
     compromised_user_file.write(user)
+
+with open('boss_message.json', 'w') as boss_message:
+  boss_message_dict = {
+    'recipient': 'The Boss',
+    'message': 'Mission Success'
+  }
+  json.dump(boss_message_dict, boss_message)
